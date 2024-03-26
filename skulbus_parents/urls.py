816212d13a5aa60cc20c8c3fbf3b0e3accfa573c
frdrckaman <frdrckaman@gmail.com>
2024-03-27 00:58:13 +0300

    update urls & main-menu

--- skulbus_parents/urls.py
+++ skulbus_parents/urls.py
@@ -1,10 +1,10 @@
 from django.urls.conf import path
 from django.views.generic import RedirectView
-from .admin_site import skulbus_schools_admin
+from .admin_site import skulbus_parents_admin
 
-app_name = "skulbus_schools"
+app_name = "skulbus_parents"
 
 urlpatterns = [
-    path("admin/", skulbus_schools_admin.urls),
+    path("admin/", skulbus_parents_admin.urls),
     path("", RedirectView.as_view(url="/skulbus_parents/admin/"), name="home_url"),
 ]
--- skulbus_students/urls.py
+++ skulbus_students/urls.py
@@ -1,10 +1,10 @@
 from django.urls.conf import path
 from django.views.generic import RedirectView
-from .admin_site import skulbus_schools_admin
+from .admin_site import skulbus_students_admin
 
 app_name = "skulbus_students"
 
 urlpatterns = [
-    path("admin/", skulbus_schools_admin.urls),
+    path("admin/", skulbus_students_admin.urls),
     path("", RedirectView.as_view(url="/skulbus_students/admin/"), name="home_url"),
 ]
